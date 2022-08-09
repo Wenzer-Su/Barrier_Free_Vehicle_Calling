9a74fb787080dcd404d65d4eed9b949a099e5093
Wenzer <suwenzhao@outlook.com>
2022-08-09 16:31:20 +0800

    Init with Springboot as a framework

---
+++ src/main/java/com/bfvc/BarrierFreeVehicleCallingApplication.java
@@ -0,0 +1,13 @@
+package com.bfvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierFreeVehicleCallingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarrierFreeVehicleCallingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bfvc/BarrierFreeVehicleCallingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrierFreeVehicleCallingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
